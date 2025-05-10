83b2e4db1f5bac126eacbeff9627dfa570319b99
sgds-scr <majorminecraft201283@yandex.ru>
2025-05-10 19:04:04 +0300

    some

--- dia_box.gd
+++ dia_box.gd
@@ -1,5 +1,9 @@
 extends TextureRect
 
+
+@export var coins: int
+@export var exp: int
+
 @onready var openingText = ["Hello and welcome to Z.O.N.E.!","In this game, you will have to unlock all kinds of zones!","In order to do so, you'll need to collect items located in unlocked zones, and then sell them to get EXP and coins!","You can use EXP to unlock new zones, or use coins to get upgrades.","Have fun,","-Z.O.N.E. dev team"]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +12,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$"../Coin/Label".text = str(coins)
+	$"../Exp/Label".text = str(exp)
 
 func talk():
 	for j in randi_range(0,len(openingText)):
